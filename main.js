3dc3c838dc7ed89a6554b5f0593b2da3f372c656
patrick <patrickcoleman@Patricks-MacBook-Air-2.local>
2019-02-27 19:23:08 -0500

    changes

--- main.js
+++ main.js
@@ -9,3 +9,45 @@ function scrollDown(element,num){
     scrollTop: $('.' + element).offset().top - num
   },'slow');
 }
+// icon toggling
+var toolTipBtn = document.getElementsByClassName("tool-tip-btn");
+
+var toolTip = document.getElementsByClassName("tool-tip");
+
+function toggleTip(){
+  for (var i = 0; i < toolTip.length; i++) {
+    toolTip[i].className = "tool-tip";
+  };
+
+  for (var i = 0; i < toolTipBtn.length; i++) {
+    toolTipBtn[i].className = "tool-tip-btn";
+  };
+
+  toggleThis(this,'active');
+
+  nextSiblingToggle(this,'active');
+
+}
+
+function closeTip(){
+
+  for (var i = 0; i < toolTip.length; i++) {
+    toolTip[i].className = "tool-tip";
+  };
+
+  for (var i = 0; i < toolTipBtn.length; i++) {
+    toolTipBtn[i].className = "tool-tip-btn";
+  };
+
+}
+
+for (var i = 0; i < toolTipBtn.length; i++) {
+
+  toolTipBtn[i].addEventListener("click",toggleTip,false);
+
+}
+for (var i = 0; i < toolTip.length; i++) {
+
+  toolTip[i].addEventListener("click",closeTip,false);
+
+}
